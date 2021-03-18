d84288ed763607b8859dacba92c2bbb3d2ee3c77
Greg-one <sgmdoc@yandex.ru>
2021-03-18 22:03:10 +0500

    feat: add router templates

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.get('/', (req, res) => {
+  res.send(`
+  <h1>HELLO</h1>
+  `);
+});
+
 app.listen(PORT, () => {
   console.log(`Server starts on port ${PORT}`);
 });
