502a3d9925c949866a21269a204dee3d50cf2dc5
Greg-one <sgmdoc@yandex.ru>
2021-04-03 12:52:33 +0500

    feat: remove hardcoded middleware

--- app.js
+++ app.js
@@ -20,14 +20,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '60571d4303646a287823a59a',
-  };
-
-  next();
-});
-
 app.use('/', usersRoute, cardRoute);
 
 app.use(helmet());
