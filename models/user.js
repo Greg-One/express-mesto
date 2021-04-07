11440770b241e89e5af9fbad0cd8895eb777d601
Greg-one <sgmdoc@yandex.ru>
2021-04-07 21:44:50 +0500

    feat: change avatar validation regExp

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(v) {
-        return /^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?$/.test(
+        return /^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?/.test(
           v,
         );
       },
