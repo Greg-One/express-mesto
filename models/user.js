3b24f4316bc372e96a2b3b08fd9d2d4651b2445d
Greg-one <sgmdoc@yandex.ru>
2021-04-08 23:11:47 +0500

    feat: change user model avatar validation

--- models/user.js
+++ models/user.js
@@ -21,11 +21,7 @@ const userSchema = new mongoose.Schema({
     default:
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator(v) {
-        return /^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?/.test(
-          v,
-        );
-      },
+      validator: (v) => /^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?/.test(v),
       message: 'URL is not valid',
     },
   },
