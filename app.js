ca7d64a7374b00e560297f2fa3a07b1d7af0cef3
Grigory <sgmdoc@yandex.ru>
2021-04-08 11:04:44 +0500

    feat: add celebrate validation to signup & signin routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi } = require('celebrate');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -24,8 +25,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.post('/signup', createUser);
-app.post('/signin', loginUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(40),
+    about: Joi.string().required().min(2).max(200),
+    avatar: Joi.string().required().pattern(/^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?/),
+  }),
+}), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), loginUser);
 
 app.use('/users', auth, usersRoute);
 app.use('/cards', auth, cardRoute);
