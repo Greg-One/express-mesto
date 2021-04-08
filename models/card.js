98a1b30621e1b5c2f04e43471e1a251aa5f252d5
Greg-one <sgmdoc@yandex.ru>
2021-04-08 22:32:23 +0500

    feat: add required mongo statuses

--- models/card.js
+++ models/card.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Card title is required.'],
     minlength: 2,
     maxlength: 30,
   },
   link: {
     type: String,
-    required: true,
+    required: [true, 'Card image link is required.'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
