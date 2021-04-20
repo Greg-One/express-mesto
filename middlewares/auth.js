3653b2ea7199aea3ed877d9d94928033c5a60801
Greg-one <sgmdoc@yandex.ru>
2021-04-20 20:12:39 +0500

    feat: change jwt

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 const ForbiddenError = require('../errors/forbidden-error');
 
-const { JWT_SECRET, NODE_ENV } = process.env;
+const { JWT_SECRET = 'such-key' } = process.env;
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'such-key');
+    // payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'such-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     next(new ForbiddenError('Authorisation error.'));
   }
