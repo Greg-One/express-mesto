cd51e1a9b8a61244f5c18215eb7981119381910c
Greg-one <sgmdoc@yandex.ru>
2021-04-22 14:08:02 +0500

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,37 @@
+// const jwt = require('jsonwebtoken');
+// const AuthorisationError = require('../errors/authorisation-error');
+
+// const { JWT_SECRET, NODE_ENV } = process.env;
+
+// const auth = (req, res, next) => {
+//   const token = req.cookies.jwt;
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'such-key');
+//   } catch (err) {
+//     next(new AuthorisationError('Authorisation error'));
+//   }
+
+//   req.user = payload;
+//   next();
+// };
+
+// module.exports = auth;
+
 const jwt = require('jsonwebtoken');
 const AuthorisationError = require('../errors/authorisation-error');
 
-const { JWT_SECRET, NODE_ENV } = process.env;
+const { JWT_SECRET = 'dev-secret' } = process.env;
 
-const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
-
+function auth(req, res, next) {
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'such-key');
+    const payload = jwt.verify(req.cookies.jwt, JWT_SECRET);
+    req.user = payload;
+    next();
   } catch (err) {
-    next(new AuthorisationError('Authorisation error'));
+    next(new AuthorisationError('Неправильный токен авторизации'));
   }
-
-  req.user = payload;
-  next();
-};
+}
 
 module.exports = auth;
